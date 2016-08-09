cec21d12fd150c007a92f8b36a219b3a52800154
Hoang Nam <namhoang.td09@gmail.com>
2016-08-09 19:28:02 +0700

    add graph

--- viv.py
+++ viv.py
@@ -26,4 +26,24 @@ print test_target
 # print test_data
 clf.predict(test_data)
 
+# viv code
+from sklearn.externals.six import StringIO
+# import pydot
+from IPython.display import Image  
+import pydotplus
+dot_data = StringIO()
+tree.export_graphviz(clf,
+        out_file=dot_data,
+        feature_names=iris.feature_names,
+        class_names=iris.target_names,
+        filled=True, rounded=True,
+        impurity=False)
+
+graph = pydotplus.graph_from_dot_data(dot_data.getvalue())
+# graph.write_pdf("iris.pdf")
+
+# print graph
+# print dir(graph)
+graph.write_pdf("iris.pdf") 
+# Image(graph.create_png())  
 
